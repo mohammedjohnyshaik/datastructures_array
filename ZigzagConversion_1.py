dfeaf3d158766a17c3a9f2783ba708c0dc8b473d
mohammedjohnysk <mohammedjohnysk@gmail.com>
2023-11-16 12:03:42 -0600

    modified

--- ZigzagConversion_1.py
+++ ZigzagConversion_1.py
@@ -3,26 +3,21 @@ class Solution:
         # Check edge cases where numRows is 1 or greater than or equal to the length of the string
         if numRows == 1 or numRows >= len(s):
             return s
-
         # Initialize an array to represent each row
         result = [''] * numRows
         index = 0
-        direction = 1  # Direction variable to determine whether to move up or down in rows
-
+        direction = 1  
         # Iterate through each character in the string
         for char in s:
             # Place the character in the current row
             result[index] += char
-
             # Change the direction when reaching the first or last row
             if index == 0:
                 direction = 1
             elif index == numRows - 1:
                 direction = -1
-
             # Move to the next row based on the direction
             index += direction
-
         # Concatenate the rows to get the final zigzag pattern
         final_result = ''.join(result)
         return final_result
